4498355586014f12485ccf4a2b714dd113440547
Gabriel <gabriel.developer@outlook.com>
2020-03-28 16:05:59 -0300

    criação da table utilizando knex

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,19 +1,16 @@
 const express = require('express');
 
-const app = express();
+const routes = express.Router();
 
-app.use(express.json());
-
-app.get('/users/', (request, response) => {
+routes.get('/users/', (request, response) => {
 
     const body = request.body;
-
     console.log(body);
-
     return response.json({
         evento: 'Semana Omnistack 11.0',
         aluno: 'Gabriel Pereira Ribeiro'
     });
 });
 
-app.listen(3333);
+
+module.exports = routes;
