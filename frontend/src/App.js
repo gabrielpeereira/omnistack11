653fb2cb227d3fd32634493648880ad029c6f089
Gabriel <gabriel.developer@outlook.com>
2020-03-29 13:19:39 -0300

    estilizando a tela de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,10 @@
-import React, { useState } from 'react';
-import Header from './Header';
+import React from 'react';
+import Logon from './pages/Logon';
+import './global.css';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function increment(){
-    setCounter(counter +1 )
-    console.log(counter);
-  }
-
   return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
-    
+    <Logon />
   );
 }
 
