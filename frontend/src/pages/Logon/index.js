693a272122332c256bec21008bab31fa99a2f515
Gabriel <gabriel.developer@outlook.com>
2020-03-29 16:00:00 -0300

    estilizando parte decadastro

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi'
 import './styles.css';
 
@@ -14,10 +15,11 @@ export default function Logon(){
                 <h1>Faça seu logon</h1>
                 <input placeholder="Seu ID" />
                 <button className="button" stype="submit">Entrar</button>
-                <a href="/register">
+                
+                <Link className="back-link" to="/register">
                 <FiLogIn size={16} color="#E02041"/>
                     Não tenho cadastro
-                </a>
+                </Link>
             </form>
             </section>
            <img src={heroesImg} alt="Heroes" />
