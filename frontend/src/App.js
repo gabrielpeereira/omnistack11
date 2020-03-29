00f935afc59b49dfb695301a94a13a2e847642fc
Gabriel <gabriel.developer@outlook.com>
2020-03-29 11:36:40 -0300

    entendo a atualização de estado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function increment(){
+    setCounter(counter +1 )
+    console.log(counter);
+  }
+
   return (
-    <h1>Hello world</h1>
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
+    
   );
 }
 
